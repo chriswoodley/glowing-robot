44d2251e49dc2aafc6832d12a4c17835ae68f9a8
Chris Woodley <chriswoodley@me.com>
2023-03-11 12:02:20 -0500

    * setup styled components in next.js

--- apps/cwoodley-com/frontend/pages/_app.js
+++ apps/cwoodley-com/frontend/pages/_app.js
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from '../components/global-styles'
 import '../styles/globals.css'
 
+const theme = {}
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
+</>
 }
