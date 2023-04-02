1c130f147b46f7c910728a2422e6e0374e78cb43
Chris Woodley <chriswoodley@me.com>
2023-04-02 15:17:18 -0400

    * add hamburger button component * add hamburger button story * add default button styles * add and export sass variables for borders, colors, and motion

---
+++ packages/design-system/src/atoms/hamburger-button/index.theme.js
@@ -0,0 +1,9 @@
+import colors from 'styles/utils/color/_variables.scss';
+
+export default {
+  standard: {
+    default: {
+      color: colors.black
+    }
+  }
+};
--- packages/design-system/src/theme.js
+++ packages/design-system/src/theme.js
@@ -1,2 +1,5 @@
-export const theme = {
+import HamburgerButton from 'atoms/hamburger-button/index.theme';
+
+export default {
+  HamburgerButton
 };
