6520d73d9be3e96d386076f0561642217ad26180
Chris Woodley <chriswoodley@me.com>
2023-04-13 08:23:52 -0400

    * create Icon component * setup icon sprite * add some icons * add global styles and handle scss variable overrides * add merriweather font * add favicon & logo svg

--- apps/custom/src/App.js
+++ apps/custom/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './styles.css';
 import { square } from './utils/math';
 import { Button } from '@cw/design-system';
-import "@cw/design-system/dist/styles.css";
+import './custom.scss';
+// import './styles.css';
 
 const App = () => {
   const value = square(4);
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <Button label={`App ${value}`} />
-      <div>hello world</div>
+      <div>hello <a href="#">world</a></div>
     </>
   );
 }
--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -39,12 +39,35 @@ module.exports = {
       styles: path.resolve(__dirname, "../src/styles/"),
       utils: path.resolve(__dirname, "../src/utils/"),
       hooks: path.resolve(__dirname, "../src/hooks/"),
+      icons: path.resolve(__dirname, "../src/icons/"),
     }
 
     config.stats = {
       ...config.stats,
       loggingDebug: ["sass-loader"],
     }
+
+    // remove svg from file loader
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.test.toString().includes('svg')) {
+        const test = rule.test.toString().replace('svg|', '').replace(/\//g, '')
+        return { ...rule, test: new RegExp(test) }
+      } else {
+        return rule
+      }
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: 'svg-sprite-loader',
+          options: {
+            symbolId: 'svg[contenthash:6]'
+          },
+        }
+      ]
+    });
 
     // Return the altered config
     return config;
