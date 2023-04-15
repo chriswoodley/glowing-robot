9739fb0f7de588c143201b10d4c2b31d298fe2ca
Chris Woodley <chriswoodley@me.com>
2023-04-15 16:16:02 -0400

    * upgrade yarn * upgrade some packages * fix webpack config

--- apps/cwoodley-com/frontend/pages/_document.js
+++ apps/cwoodley-com/frontend/pages/_document.js
@@ -1,26 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
 
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      }
+        styles: [ initialProps.styles, sheet.getStyleElement() ],
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="icon" type="image/png" href="/favicon.png"/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         </Head>
         <body>
@@ -39,6 +39,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
