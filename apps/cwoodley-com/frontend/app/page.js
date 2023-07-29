837991b9e610ba1590e121dd53096010bb8af4bf
Chris Woodley <chriswoodley@me.com>
2023-07-29 15:15:27 -0400

    * install swiper * add initial carousel component and content

--- apps/cwoodley-com/frontend/app/page.js
+++ apps/cwoodley-com/frontend/app/page.js
@@ -1,13 +1,73 @@
-import React from 'react';
+'use client';
 
-export const metadata = {
-  title: 'Chris Woodley',
-  description: 'Chris Woodley'
-};
+import React from 'react';
+import Carousel from 'components/carousel';
+import { SwiperSlide } from 'swiper/react';
+import {
+  Image,
+  AspectRatioContainer
+} from '@cw/design-system';
 
 function RootPage() {
   return (
       <>
+        <Carousel>
+          <SwiperSlide>
+            <AspectRatioContainer
+              size="xlg"
+              ratio="3/2"
+            >
+              <Image
+                src="projects/losant-com-workflow-1280.png"
+                alt="Losant"
+                width="1280"
+                height="836"
+              />
+            </AspectRatioContainer>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <AspectRatioContainer
+              size="xlg"
+              ratio="3/2"
+            >
+              <Image
+                src="../projects/genetesis-com.png"
+                alt="Genetesis"
+                width="1761"
+                height="1307"
+              />
+            </AspectRatioContainer>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <AspectRatioContainer
+              size="xlg"
+              ratio="3/2"
+            >
+              <Image
+                src="projects/mybpstation-com.jpg"
+                alt="My BP Station"
+                width="1263"
+                height="929"
+              />
+            </AspectRatioContainer>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <AspectRatioContainer
+              size="xlg"
+              ratio="3/2"
+            >
+              <Image
+                src="projects/tealeaveshealth-com.jpg"
+                alt="Tea Leaves Health"
+                width="1263"
+                height="929"
+              />
+            </AspectRatioContainer>
+          </SwiperSlide>
+        </Carousel>
         <h1>Services</h1>
 
         <ul className="pl-5">
